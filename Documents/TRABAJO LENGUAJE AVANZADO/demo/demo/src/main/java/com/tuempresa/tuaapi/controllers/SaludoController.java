61cbeb0f07c3f015f33c1d12f10c633ff9247e3a
SteveJobssss <edwardse1491@gmail.com>
2025-03-01 00:02:46 -0500

    Ejercicio 3: Implementar Internacionalización (i18n) - Solucionado con mensajes en inglés y español

--- Documents/TRABAJO LENGUAJE AVANZADO/demo/demo/src/main/java/com/tuempresa/tuaapi/controllers/SaludoController.java
+++ Documents/TRABAJO LENGUAJE AVANZADO/demo/demo/src/main/java/com/tuempresa/tuaapi/controllers/SaludoController.java
@@ -1,14 +1,23 @@
 package com.tuempresa.tuaapi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/api")
 public class SaludoController {
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("/saludo")
-    public String obtenerSaludo() {
-        return "¡Hola, API RESTful!";
+    public String obtenerSaludo(@RequestParam(name = "lang", defaultValue = "en") String lang) {
+        Locale locale = new Locale(lang);
+        return messageSource.getMessage("saludo", null, locale);
     }
 }
